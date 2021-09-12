34ab1a76242ce2b0d2e527ed6ccbcf9753333abb
Nelson Esmarlin Rosario Rodriguez <esmarlin9@gmail.com>
2021-09-11 22:58:20 -0400

    Add create color endpoint

--- Controllers/ColorController.cs
+++ Controllers/ColorController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProductsAdmin.Database.Contexts;
+using ProductsAdmin.Database.Models;
 using ProductsAdmin.Models.Api;
+using ProductsAdmin.Models.Api.Requests;
 using ProductsAdmin.Models.Api.Responses;
 using System;
 using System.Collections.Generic;
@@ -39,9 +41,9 @@ namespace ProductsAdmin.Controllers
                     StatusName = e.StatusNavigation.Name,
                     Value = e.Value
                 })
-                .FirstOrDefault(e=>e.Id==id);
+                .FirstOrDefault(e => e.Id == id);
 
-            if(dbColor== null)
+            if (dbColor == null)
             {
                 var errorResponse = new RestErrorResponse();
                 errorResponse.ErrorType = "resource_not_found";
@@ -80,10 +82,46 @@ namespace ProductsAdmin.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create()
+        public async Task<IActionResult> Create([FromBody] CreateColorRequest request)
         {
-            await Task.FromResult(0);
-            return Ok();
+
+            try
+            {
+                var color = new Color();
+                color.Name = request.Name;
+                color.Value = request.Value;
+                color.Format = request.Format;
+                color.Status = request.StatusId;
+
+                var entity = context.Colors.Add(color).Entity;
+                await context.SaveChangesAsync();
+
+                var result = context.Colors
+                    .Include(e => e.StatusNavigation)
+                .Select(e => new ColorRestResponse
+                {
+                    Id = e.Id,
+                    Name = e.Name,
+                    Format = e.Format,
+                    CreatedAt = e.CreatedAt,
+                    StatusId = e.StatusNavigation.Id,
+                    StatusName = e.StatusNavigation.Name,
+                    Value = e.Value
+                })
+                .FirstOrDefault(e => e.Id == entity.Id);
+
+                var response = new RestOkResponse<ColorRestResponse>(result);
+
+                return StatusCode(201, response);
+            }
+            catch (Exception e)
+            {
+                var errorResponse = new RestErrorResponse();
+                errorResponse.ErrorType = "server_error";
+                errorResponse.Error = $"Server can´t handle the request";
+
+                return StatusCode(500, errorResponse);
+            }
         }
 
         [HttpPut]
