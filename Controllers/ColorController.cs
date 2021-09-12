da2d78e85cdaa5e31577e37d98af735d4ba753de
Nelson Esmarlin Rosario Rodriguez <esmarlin9@gmail.com>
2021-09-11 21:18:33 -0400

    Create template for Color endpoints for api v1.

---
+++ Controllers/ColorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAdmin.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ColorController : ControllerBase
+    {
+
+        public async Task<IActionResult> GetAll()
+        {
+            await Task.FromResult(0);
+            return Ok();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            await Task.FromResult(0);
+            return Ok();
+        }
+
+        public async Task<IActionResult> Update()
+        {
+            await Task.FromResult(0);
+            return Ok();
+        }
+
+    }
+}
