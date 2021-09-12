f0d6b503b1f3947517c614c543a1784557ea5db4
Nelson Esmarlin Rosario Rodriguez <esmarlin9@gmail.com>
2021-09-11 23:34:41 -0400

    Add update to Color endpoint

--- Controllers/ColorController.cs
+++ Controllers/ColorController.cs
@@ -27,58 +27,80 @@ namespace ProductsAdmin.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
-            await Task.FromResult(0);
 
-            var dbColor = context.Colors
-                .Include(e => e.StatusNavigation)
-                .Select(e => new ColorRestResponse
+            try
+            {
+                var dbColor = context.Colors
+                        .Include(e => e.StatusNavigation)
+                        .Select(e => new ColorRestResponse
+                        {
+                            Id = e.Id,
+                            Name = e.Name,
+                            Format = e.Format,
+                            CreatedAt = e.CreatedAt,
+                            StatusId = e.StatusNavigation.Id,
+                            StatusName = e.StatusNavigation.Name,
+                            Value = e.Value
+                        })
+                        .FirstOrDefault(e => e.Id == id);
+
+                if (dbColor == null)
                 {
-                    Id = e.Id,
-                    Name = e.Name,
-                    Format = e.Format,
-                    CreatedAt = e.CreatedAt,
-                    StatusId = e.StatusNavigation.Id,
-                    StatusName = e.StatusNavigation.Name,
-                    Value = e.Value
-                })
-                .FirstOrDefault(e => e.Id == id);
+                    var errorResponse = new RestErrorResponse();
+                    errorResponse.ErrorType = "resource_not_found";
+                    errorResponse.Error = $"Color with <Id> -> <{id}> doesn´t exists";
 
-            if (dbColor == null)
+                    return NotFound(errorResponse);
+                }
+
+
+                var response = new RestOkResponse<ColorRestResponse>(dbColor);
+
+                return Ok(response);
+            }
+            catch (Exception e)
             {
                 var errorResponse = new RestErrorResponse();
-                errorResponse.ErrorType = "resource_not_found";
-                errorResponse.Error = $"Color with <Id> -> <{id}> doesn´t exists";
+                errorResponse.ErrorType = "server_error";
+                errorResponse.Error = $"Server can´t handle the request";
 
-                return NotFound(errorResponse);
+                return StatusCode(500, errorResponse);
             }
-
-
-            var response = new RestOkResponse<ColorRestResponse>(dbColor);
-
-            return Ok(response);
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            await Task.FromResult(0);
+            try
+            {
+                await Task.FromResult(0);
 
-            var result = await context.Colors
-                    .Include(e => e.StatusNavigation)
-                .Select(e => new ColorRestResponse
-                {
-                    Id = e.Id,
-                    Name = e.Name,
-                    Format = e.Format,
-                    CreatedAt = e.CreatedAt,
-                    StatusId = e.StatusNavigation.Id,
-                    StatusName = e.StatusNavigation.Name,
-                    Value = e.Value
-                }).ToListAsync();
+                var result = await context.Colors
+                        .Include(e => e.StatusNavigation)
+                    .Select(e => new ColorRestResponse
+                    {
+                        Id = e.Id,
+                        Name = e.Name,
+                        Format = e.Format,
+                        CreatedAt = e.CreatedAt,
+                        StatusId = e.StatusNavigation.Id,
+                        StatusName = e.StatusNavigation.Name,
+                        Value = e.Value
+                    }).ToListAsync();
 
-            var response = new RestOkResponse<List<ColorRestResponse>>(result);
+                var response = new RestOkResponse<List<ColorRestResponse>>(result);
 
-            return Ok(response);
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+
+                var errorResponse = new RestErrorResponse();
+                errorResponse.ErrorType = "server_error";
+                errorResponse.Error = $"Server can´t handle the request";
+
+                return StatusCode(500, errorResponse);
+            }
         }
 
         [HttpPost]
@@ -124,11 +146,58 @@ namespace ProductsAdmin.Controllers
             }
         }
 
-        [HttpPut]
-        public async Task<IActionResult> Update()
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id,[FromBody] UpdateColorRequest request)
         {
-            await Task.FromResult(0);
-            return Ok();
+
+            try
+            {
+                var color = context.Colors
+                        .FirstOrDefault(e => e.Id == id);
+
+                if (color == null)
+                {
+                    var errorResponse = new RestErrorResponse();
+                    errorResponse.ErrorType = "resource_not_found";
+                    errorResponse.Error = $"Color with <Id> -> <{id}> doesn´t exists";
+
+                    return NotFound(errorResponse);
+                }
+
+                color.Name = request.Name;
+                color.Value = request.Value;
+                color.Format = request.Format;
+                color.Status = request.StatusId;
+
+                context.Entry(color).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+
+                context.Entry(color).Reference(e=>e.StatusNavigation).Load();
+
+                var result = new ColorRestResponse
+                {
+                    Id = color.Id,
+                    Name = color.Name,
+                    Format = color.Format,
+                    CreatedAt = color.CreatedAt,
+                    StatusId = color.StatusNavigation.Id,
+                    StatusName = color.StatusNavigation.Name,
+                    Value = color.Value
+                };
+
+
+                var response = new RestOkResponse<ColorRestResponse>(result);
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                var errorResponse = new RestErrorResponse();
+                errorResponse.ErrorType = "server_error";
+                errorResponse.Error = $"Server can´t handle the request";
+
+                return StatusCode(500, errorResponse);
+            }
         }
 
     }
